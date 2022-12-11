7d739bdde9ff0fa4300e0e90bb3b03945af4ed0f
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-12-12 02:15:27 +0700

    basic update

--- ruok/lib/main.dart
+++ ruok/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ruok/pages/mh_tests.dart';
+import 'package:ruok/pages/events_page.dart';
+import 'package:ruok/pages/stories.dart';
+import 'package:ruok/main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,36 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tests',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Tests'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -57,7 +42,10 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const TestPage()),
+      );
     });
   }
 
---
+++ ruok/lib/pages/mh_tests.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:ruok/pages/events_page.dart';
+import 'package:ruok/pages/stories.dart';
+import 'package:ruok/main.dart';
+
+class TestPage extends StatefulWidget {
+  const TestPage({super.key});
+  @override
+  State<TestPage> createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> oftenSad = ['No', 'Maybe', 'Yes'];
+  List<String> oftenTired = ['No', 'Maybe', 'Yes'];
+  List<String> oftenCry = [
+    'Less than 3 times',
+    '3-5 times',
+    'More than 5 times',
+    'Nearly every day'
+  ];
+  List<String> oftenLonely = [
+    'I feel happy to have so many friends here.',
+    'Sometimes I feel alone but there are still some people I can rely on',
+    'Yes, I am alone and I do not trust other people'
+  ];
+  List<String> oftenSuicidal = [
+    'I am happy with my journey and I want to live a healthy life.',
+    'Even though my life is hard sometimes, I do not think about ending it.',
+    'I have had suicidal thoughts once in a while.',
+    ' Lately I really want to end my life.'
+  ];
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Tests'),
+        ),
+        trailing: DropdownButton(
+                          value: kelasPBP,
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          items: listKelasPBP.map((String items) {
+                            return DropdownMenuItem(
+                              value: items,
+                              child: Text(items),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              kelasPBP = newValue!;
+                            });
+                          },
+                        ),
+                      ),// This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
